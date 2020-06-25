96ea2640771afff87f741d4e000da5ebd11db884
LuisRenePanjon <lucho@LAPTOP-1AESCG16>
2020-06-24 20:51:22 -0500

    Creacion de entidades.

---
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/entidad/Distribuidora.java
@@ -0,0 +1,104 @@
+package ec.edu.ups.entidad;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Distribuidora
+ *
+ */
+@Entity
+public class Distribuidora implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int codigo;
+	
+	private String nombre;
+	
+	private String direccion;
+	
+	private String ruc;
+	
+	
+	public Distribuidora() {
+		super();
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getRuc() {
+		return ruc;
+	}
+
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Distribuidora other = (Distribuidora) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Distribuidora [codigo=" + codigo + ", nombre=" + nombre + ", direccion=" + direccion + ", ruc=" + ruc
+				+ "]";
+	}
+	
+	
+   
+}
