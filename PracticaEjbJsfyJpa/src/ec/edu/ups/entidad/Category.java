73e3418f4fed2780fac4cabb5ab2649d0ba5ec12
LuisRenePanjon <lucho@LAPTOP-1AESCG16>
2020-06-25 12:00:46 -0500

    Creacion de bean para corregir errores de versiones.

---
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/entidad/Category.java
@@ -0,0 +1,16 @@
+package ec.edu.ups.entidad;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+import javax.inject.Named;
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@Named
+@RequestScoped
+public class Category {
+
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
