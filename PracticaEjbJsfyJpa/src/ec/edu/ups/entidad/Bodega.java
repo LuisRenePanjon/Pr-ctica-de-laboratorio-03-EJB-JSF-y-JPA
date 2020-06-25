babdb65719e91c8b0e122c95ce64622b4262a41d
XAVIEERBBC <bryan19972011@hotmail.es>
2020-06-25 15:28:09 -0500

    Creando Entidades Bodega y Ubicacion

--- PracticaEjbJsfyJpa/src/ec/edu/ups/entidad/Bodega.java
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/entidad/Bodega.java
@@ -1,6 +1,10 @@
 package ec.edu.ups.entidad;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.*;
 
 /**
@@ -17,9 +21,85 @@ public class Bodega implements Serializable {
 	@Id
 	private int codigo;
 	private String nombre;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "bodega")
+	private Set<Ubicacion> ubicaciones = new HashSet<Ubicacion>();
+	
+	
 
 	public Bodega() {
 		super();
 	}
+
+
+
+	public Bodega(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public Set<Ubicacion> getUbicaciones() {
+		return ubicaciones;
+	}
+
+
+
+	public void setUbicaciones(Set<Ubicacion> ubicaciones) {
+		this.ubicaciones = ubicaciones;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bodega other = (Bodega) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+	
+	
    
+	
 }
