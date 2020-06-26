9029fd8d677e6e42ce6f7ca1c0e158dbecc5cb05
pabloAMD <pablo.malla.abc.com@gmail.com>
2020-06-26 16:56:50 -0500

    Modificacion Clase General

--- PracticaEjbJsfyJpa/src/ec/edu/ups/controlador/General.java
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/controlador/General.java
@@ -2,10 +2,16 @@ package ec.edu.ups.controlador;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 
 @Named
 @SessionScoped
-public class General {
+public class General implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public General() {
 		// TODO Auto-generated constructor stub
