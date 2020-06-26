69bd4a3898106efd84ecff66a1e12268e68aa506
Christian <criveral2@est.ups.edu.ec>
2020-06-26 15:50:24 -0500

    Creacion del paquete controlaor

---
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/controlador/General.java
@@ -0,0 +1,14 @@
+package ec.edu.ups.controlador;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class General {
+
+	public General() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
