85aa7379ad4dc9159c8bec4ec755ca6965caa82a
XAVIEERBBC <bryan19972011@hotmail.es>
2020-07-21 13:50:12 -0500

    Bodega - Distribuidora Interfaz

--- PracticaEjbJsfyJpa/src/ec/edu/ups/controlador/DistribuidoraBean.java
+++ PracticaEjbJsfyJpa/src/ec/edu/ups/controlador/DistribuidoraBean.java
@@ -40,6 +40,46 @@ public class DistribuidoraBean implements Serializable{
 	private String ruc;
 	
 	
+	public DistribuidoraFacade getDistribuidoraFacade() {
+		return distribuidoraFacade;
+	}
+
+	public void setDistribuidoraFacade(DistribuidoraFacade distribuidoraFacade) {
+		this.distribuidoraFacade = distribuidoraFacade;
+	}
+
+	public List<Distribuidora> getList() {
+		return list;
+	}
+
+	public void setList(List<Distribuidora> list) {
+		this.list = list;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+
 	public DistribuidoraBean() {
 		
 	}
